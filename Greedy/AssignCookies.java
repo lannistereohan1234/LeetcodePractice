b16466124d2cf773388a4508de071e7873f58ce0
Rohan Kumar <rohan.kraug20@gmail.com>
2026-05-08 12:28:05 +0530

    Update AssignCookies.java

--- Greedy/AssignCookies.java
+++ Greedy/AssignCookies.java
@@ -1,22 +1,46 @@
-class Solution {
+import java.util.Arrays;
+
+// similar problems:
+// https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+// https://leetcode.com/problems/maximum-ice-cream-bars/description/
+// https://leetcode.com/problems/boats-to-save-people/description/
+// https://leetcode.com/problems/bag-of-tokens/description/
+// https://leetcode.com/problems/candy/description/
+
+public class AssignCookies {
     public int findContentChildren(int[] g, int[] s) {
-         Arrays.sort(g);
-         Arrays.sort(s);
+        // Step 1: Sort both arrays to use the greedy approach
+        Arrays.sort(g);
+        Arrays.sort(s);
 
-         int i=0;
-         int j=0;
+        int childPointer = 0;
+        int cookiePointer = 0;
 
-        int count=0;
-
-         while(i<g.length && j<s.length){
-            if(s[j]>=g[i]){
-                count++;
-                i++;
+        // Step 2: Iterate through both arrays
+        while (childPointer < g.length && cookiePointer < s.length) {
+            // If the current cookie can satisfy the current child
+            if (s[cookiePointer] >= g[childPointer]) {
+                childPointer++; // Move to the next child
             }
-                j++;
-            
-         }
+            // Move to the next cookie regardless of whether it was used
+            cookiePointer++;
+        }
 
-         return count;
+        // The index of childPointer represents the number of satisfied children
+        return childPointer;
+    }
+
+    public static void main(String[] args) {
+        AssignCookies sol = new AssignCookies();
+
+        // Example 1
+        int[] g1 = {1, 2, 3};
+        int[] s1 = {1, 1};
+        System.out.println("Result 1: " + sol.findContentChildren(g1, s1)); // Output: 1
+
+        // Example 2
+        int[] g2 = {1, 2};
+        int[] s2 = {1, 2, 3};
+        System.out.println("Result 2: " + sol.findContentChildren(g2, s2)); // Output: 2
     }
 }
